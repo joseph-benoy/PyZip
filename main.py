46bd958a79783438b36b8d83b52f3aec25accb50
joseph-benoy <josephbenoy03@gmail.com>
2020-05-17 08:18:33 +0530

    Added functionalities

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class GUIForm(Ui_MainWindow):
 	def __init__(self,window):
 		self.setupUi(window)
 		self.NewList.setColumnWidth(0,400)
+		self.ExtractList.setColumnWidth(0,250)
+		self.FileInfoList.setColumnWidth(0,250)
 		self.window = window
 		self.FileCount=0
 		self.AddFileBtn.clicked.connect(self.addFileToList)
@@ -18,6 +20,10 @@ class GUIForm(Ui_MainWindow):
 		self.ClearListBtn.clicked.connect(self.clearList)
 		self.CreateZipBtn.clicked.connect(self.createZip)
 		self.NewSelectDestBtn.clicked.connect(self.NewSelectDest)
+		self.ExtractSelectBtn.clicked.connect(self.ExtSelectFile)
+		self.ExtractDestBtn.clicked.connect(self.ExtSelectDest)
+		self.ExtractBtn.clicked.connect(self.Extract)
+		self.FileSelectBtn.clicked.connect(self.SelectFile)
 	def addFileToList(self,window):
 		fpath = QFileDialog.getOpenFileNames(self.window,'Add file','',"All files (*.*)")
 		files= fpath[0]
@@ -69,8 +75,67 @@ class GUIForm(Ui_MainWindow):
 				self.NewSelectDest()
 		except Exception as error:
 			QMessageBox.warning(self.window,'Error!','Something went wrong!')
+	def ExtSelectFile(self):
+		try:
+			fname = QFileDialog.getOpenFileName(self.window,'Open a Zip','','Zip files (*.zip)')
+			fname = fname[0]
+			self.ExtractFileInput.setText(fname)
+			zip = zipfile.ZipFile(fname,'r',allowZip64=True)
+			FilesInZip = zip.infolist()
+			FileList = []
+			for file in FilesInZip:
+				z=[]
+				z.append(file.filename)
+				date = file.date_time
+				date = '{}/{}/{}'.format(date[2],date[1],date[0])
+				z.append(date)
+				z.append(str(file.file_size)+' Bytes')
+				FileList.append(z)
+			self.ExtractList.clear()
+			for fileInfo in FileList:
+				item = QTreeWidgetItem(self.ExtractList,fileInfo)
+		except Exception as error:
+			QMessageBox.warning(self.window,'Error!','Something went wrong!')
+	def ExtSelectDest(self):
+		try:
+			fname = QFileDialog.getExistingDirectory(self.window,'Select destination','')
+			self.ExtractDestInput.setText(fname)
+		except:
+			QMessageBox.warning(self.window,'Error!','Something went wrong!')
+	def Extract(self):
+		src = self.ExtractFileInput.text()
+		dest = self.ExtractDestInput.text()
+		if(src==''):
+			self.ExtSelectFile()
+		if(dest==''):
+			self.ExtSelectDest()
+		try:
+			zip = zipfile.ZipFile(dest,'r',allowZip64=True)
+			zip.extractall(path=dest)
+		except Exception as error:
+			print(error)
+	def SelectFile(self):
+		try:
+			fname = QFileDialog.getOpenFileName(self.window,'Open a Zip','','Zip files (*.zip)')
+			fname = fname[0]
+			self.FileInfoInput.setText(fname)
+			zip = zipfile.ZipFile(fname,'r',allowZip64=True)
+			FilesInZip = zip.infolist()
+			FileList = []
+			for file in FilesInZip:
+				z=[]
+				z.append(file.filename)
+				date = file.date_time
+				date = '{}/{}/{}'.format(date[2],date[1],date[0])
+				z.append(date)
+				z.append(str(file.file_size)+' Bytes')
+				FileList.append(z)
+			self.ExtractList.clear()
+			for fileInfo in FileList:
+				item = QTreeWidgetItem(self.FileInfoList,fileInfo)
+		except Exception as error:
+			QMessageBox.warning(self.window,'Error!','Something went wrong!')		
 
-		
 
 
 if __name__ == "__main__":
